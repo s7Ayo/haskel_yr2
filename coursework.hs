8ef6f5f809326d8855880821c99e0fd5eb586ad2
s7Ayo <96587946+s7Ayo@users.noreply.github.com>
2023-04-07 01:21:36 +0100

    Create coursework.hs

---
+++ coursework.hs
@@ -0,0 +1,42 @@
+  --  Function 1: Implement a function tripling a given numerical input 
+  triple :: Num a => a -> a
+  triple x = x * 3
+  
+  --  Function 2: Implement the Collatz conjecture
+  collatz :: Integral a => a -> a
+  collatz n
+    | n == 1 = 0  -- Base case: we have reached 1
+    | even n = 1 + collatz (n `div` 2)  -- n is even
+    | otherwise = 1 + collatz (3 * n + 1)  -- n is odd
+
+ 
+  -- Function 3: Implement a function that splits a list 
+  halve :: [a] -> ([a],[a])
+  halve xs = (take half xs, drop half xs)
+    where 
+      half = length xs `div` 2
+  
+
+
+  -- Function 4: Implement a function that splits a list
+
+  isLeapYear :: Integral a => a -> Bool
+  isLeapYear year
+      | year `mod` 400 == 0 = True
+      | year `mod` 100 == 0 = False
+      | year `mod` 4   == 0 = True
+      | otherwise          = False
+  
+
+  -- Function 5: Implement a function that searches a list for an item
+  find :: Eq a => a -> [a] -> Int -> Int
+  find _ [] _ = -1
+  find x (y:ys) i
+      | x == y    = i
+      | otherwise = find x ys (i + 1)
+
+  findElem :: Eq a => a -> [a] -> Int
+  findElem x ys = find x ys 0
+
+  -- Function 6: Implement a pangram checker 
+  
